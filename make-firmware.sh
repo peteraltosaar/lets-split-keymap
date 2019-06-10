84160de5b705481cc92d4c050d0e90e21956ee52
Peter Altosaar <peter.altosaar@gmail.com>
2019-06-10 00:03:00 -0400

    Expand the make script to insert password on the fly

--- make-firmware.sh
+++ make-firmware.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+echo "Password:"
+read -s password
+
 cp keymap.c ../qmk_firmware/keyboards/lets_split/keymaps/peteraltosaar/
 cd ../qmk_firmware
+sed -i '' 's/PW_PLACEHOLDER/'$password'/g' keyboards/lets_split/keyamps/peteraltosaar/keymap.c
 make lets_split/rev2:peteraltosaar
+rm keymap.c
 cd -
