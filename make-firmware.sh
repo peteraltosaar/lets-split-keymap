cb25c01f5a867b91761a2b31d7ff15e0f188d94c
Peter Altosaar <peter.alrtosaar@gmail.com>
2020-10-13 21:13:01 -0400

    Update script to work for different arduino chip flashing

--- make-firmware.sh
+++ make-firmware.sh
@@ -5,7 +5,7 @@ read -s password
 
 cp keymap.c ../qmk_firmware/keyboards/lets_split/keymaps/peteraltosaar/
 cd ../qmk_firmware
-sed -i '' 's/PW_PLACEHOLDER/'$password'/g' keyboards/lets_split/keyamps/peteraltosaar/keymap.c
-make lets_split/rev2:peteraltosaar
-rm keymap.c
+sed -i '' 's/PW_PLACEHOLDER/'$password'/g' keyboards/lets_split/keymaps/peteraltosaar/keymap.c
+make lets_split/rev2:peteraltosaar:avrdude
+rm keyboards/lets_split/keymaps/peteraltosaar/keymap.c
 cd -
